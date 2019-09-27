902850dcc6e36c2b5bcfd76c88a180fbfa5a2d01
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-27 18:11:48 +0300

    [task] use R.reduce to form an object with cost array and internetSpeeds

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ const updateTemperature = R.curry((convertFn, city) => {
 
 const updatedCities = R.map(updateTemperature(KtoF), cities);
 
-console.log(updatedCities);
+//console.log(updatedCities);
+const totalCostReducer = (acc, city) => {
+  const { cost = 0 } = city; //incase it doesn't exist, define it with zero
+  return acc + cost;
+};
+const totalCost = R.reduce(totalCostReducer, 0, updatedCities);
+const cityCount = R.length(updatedCities);
+//console.log(totalCost / cityCount);
+
+const groupByPropReducer = (acc, city) => {
+  const { cost = [], internetSpeed = [] } = acc; //if not found, set them on object
+  return R.merge(acc, {
+    cost: R.append(city.cost, cost), //add city.cost to current cost
+    internetSpeed: R.append(city.internetSpeed, internetSpeed)
+  });
+};
+
+const groupedByProp = R.reduce(groupByPropReducer, {}, updatedCities);
+console.log(groupedByProp);
